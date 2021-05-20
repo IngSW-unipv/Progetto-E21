d9c8ad6da2f04dc202f80615f3fcba230669bcfe
marcomarchesi117 <marco.marchesi04@universitadipavia.it>
2021-05-20 17:19:49 +0200

    aggiunto item e image

--- src/it/unipv/ingsfw/auction/Item.java
+++ src/it/unipv/ingsfw/auction/Item.java
@@ -1,5 +1,23 @@
 package it.unipv.ingsfw.auction;
+import java.util.ArrayList;
+import it.unipv.ingsfw.utilities.Image;
 
 public class Item {
-
+	private String name, descrizione;
+	ArrayList<Image> image;
+	public Item(String name, String descrizione, ArrayList<Image> image) {
+		super();
+		this.name = name;
+		this.descrizione = descrizione;
+		this.image = image;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public ArrayList<Image> getImage() {
+		return image;
+	}
 }
---
+++ src/it/unipv/ingsfw/utilities/Image.java
@@ -0,0 +1,5 @@
+package it.unipv.ingsfw.utilities;
+
+public class Image {
+
+}
