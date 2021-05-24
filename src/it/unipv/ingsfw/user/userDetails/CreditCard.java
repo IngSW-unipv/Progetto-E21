b04c7df688f281f8398ca4235acd9f9363a0a383
johnhax <johnhax@192.168.183.168>
2021-05-24 19:12:37 +0200

    CreditCard

--- src/it/unipv/ingsfw/user/userDetails/CreditCard.java
+++ src/it/unipv/ingsfw/user/userDetails/CreditCard.java
@@ -1,5 +1,47 @@
 package it.unipv.ingsfw.user.userDetails;
+import java.sql.Date;
 
 public class CreditCard {
+	private String number;
+	private String CVV;
+	private Date expirationDate;
+	private String firstName;
+	private String lastName;
+	
+	public CreditCard(String number, String cVV, Date expirationDate, String firstName, String lastName) {
+		
+		this.number = number;
+		this.CVV = CVV;
+		this.expirationDate = expirationDate;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public String getCVV() {
+		return CVV;
+	}
+
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
