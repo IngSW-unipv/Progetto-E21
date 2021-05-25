e2c3df17c8d3a15f46e0cab3c8f1b609eee0c8b9
crisele05 <crisele.ariola01@universitadipavia.it>
2021-05-25 15:59:43 +0200

    mysql connected eclipse

--- src/it/unipv/ingsfw/user/ParticipantRegistration.java
+++ src/it/unipv/ingsfw/user/ParticipantRegistration.java
@@ -64,19 +64,17 @@ public class ParticipantRegistration {
     
     
 	public static void main(String args[]){  
-		try{  
-		Class.forName("com.mysql.jdbc.Driver");  
-		Connection con=DriverManager.getConnection(  
-		"jdbc:mysql://localhost:3306/regitration_system","root","");  
-		//here registration_system is database name, root is username and password  
-		Statement stmt=con.createStatement();  
-		ResultSet rs=stmt.executeQuery("select * from participant");  
-		while(rs.next())  
-		System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-		con.close();  
-		}catch(Exception e){ System.out.println(e);}  
-		}  
-		} 
+	 
+			 try {
+					Connection	 connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration_system", "root", "Crisele05");//Establishing connection
+					System.out.println("Connected With the database successfully");
+		}
+			 catch (SQLException e) {
+				 System.out.println("Error while connecting to the database");
+	}
 
+	}
+}
+		
 
   
