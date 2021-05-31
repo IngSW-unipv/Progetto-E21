b8704fcb58d674bb8cf0c7b3a151e1dbf520c069
crisele05 <crisele.ariola01@universitadipavia.it>
2021-05-31 16:29:18 +0200

    participant

--- src/it/unipv/ingsfw/user/Participant.java
+++ src/it/unipv/ingsfw/user/Participant.java
@@ -1,9 +1,14 @@
 package it.unipv.ingsfw.user;
 
-import java.time.LocalDate;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.TimeZone;
 
 public class Participant {
 	
@@ -84,30 +89,193 @@ public class Participant {
 			
 			
 		}
-	
-
-	public Participant() {
+		
+		public Participant() {
 			// TODO Auto-generated constructor stub
 		}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//aggiorna
+		public static ArrayList<Participant> listOfParticipant() throws SQLException {
+			Connection cn = null;
+			Statement st;
+			ResultSet rs;
+			String sql;
+			//___________connesione___________
+			   try {
+				 
+				cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration_system", "root", "Crisele05");//Establishing connection
+				System.out.println("Connected With the database successfully");	
+			} catch (SQLException e) {
+				
+				System.out.println("Error while connecting to the database");
+			
+					}
 		
-		Participant p1 = new Participant ();
-		p1.setFirstName("CRISELE");
-		p1.setLastName("ARIOLA");
-		p1.setUsername("crisele05");
-		p1.setPassword("password");
-		p1.setMobileNumber(380000000);
-		p1.setAddress("via Rismondo 72");
-	
-		p1.setBirthday(java.sql.Date.valueOf("2013-09-04"));
+			   ArrayList<Participant> list = new ArrayList<Participant>();
+		        sql = "SELECT * FROM participant;";
+			   //____________query___________
+			   try {
+				   st = cn.createStatement(); //creo sempre uno statement sulla coneesione
+				   
+				   rs = st.executeQuery(sql); //faccio la query su uno statement
+				   while(rs.next() == true) {
+					   
+					   Participant p1 = new Participant(rs.getString("first_name"), rs.getString("last_name"), rs.getString("email"), rs.getString("username"), rs.getString("password"), rs.getString("address"), rs.getInt("mobile_number"), rs.getDate("birthday"));
+					   
+					   System.out.println(rs.getNString(1) + "\t" + rs.getNString("last_name") + rs.getDate("birthday"));
+			
+				   }
+				   
+			   } catch(SQLException e) {
+				   System.out.println("errore: " + e.getMessage());
+			   } //fine try-catch  
+			return list;
+		}
+		
+		public static void deleteParticipant(Participant p1) throws SQLException {
+			
+			
+			Connection cn = null;
+			Statement st;
+			ResultSet rs;
+			String sql;
+			
+			//___________connesione___________
+			   try {
+				 
+				cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration_system", "root", "Crisele05");//Establishing connection
+				System.out.println("Connected With the database successfully");	
+			} catch (SQLException e) {
+				
+				System.out.println("Error while connecting to the database");
+			
+					}
+			   String username = p1.getUsername();
 		
 		
+		        sql = "DELETE FROM PARTICIPANT WHERE USERNAME= " + username;
+			   //____________query___________
+			   try {
+				   st = cn.createStatement(); //creo sempre uno statement sulla coneesione
+				   
+				   rs = st.executeQuery(sql); //faccio la query su uno statement
+				   while(rs.next() == true) {
+					   
+					  
+					   
+					   System.out.println(rs.getNString(1) + "\t" + rs.getNString("username") );
+			
+				   }
+				   
+			   } catch(SQLException e) {
+				   System.out.println("errore: " + e.getMessage());
+			   } //fine try-catch  
+			   
+			   
+			   //ha dei metodi per leggere/scrivere e modificare il db registration_system
+			   
+			   
+			   cn.close();
+			   System.out.println("connection terminated"); 
+			
+		}
 		
+		public void newParticipant(Participant p1) throws SQLException {
+			
+			Connection cn = null;
+			Statement st;
+			ResultSet rs;
+			String sql;
+			
 		
-		System.out.println(p1.getBirthday());
+			   try {
+				 
+				cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/registration_system", "root", "Crisele05");//Establishing connection
+				System.out.println("Connected With the database successfully");	
+			} catch (SQLException e) {
+				
+				System.out.println("Error while connecting to the database");
+			
+					}
+			   
+			   
+			   String first_name = p1.getFirstName();
+			   String last_name = p1.getLastName();
+			   String username = p1.getUsername();
+			   String password = p1.getPassword();
+			   String email = p1.getEmail();
+			   String address = p1.getPassword();
+			   int mobile_number = p1.getMobileNumber();
+			   Calendar birthday = Calendar.getInstance();
+			   birthday.setTime(p1.getBirthday());
+			   String sqlData = birthday.get(Calendar.YEAR) + "-" + birthday.get(Calendar.MONTH) + "-" + birthday.get(Calendar.DAY_OF_MONTH);
+			   
+		
+		       
+			   
+			     try {
+			    	  // Statements allow to issue SQL queries to the database
+			            st = cn.createStatement();
+			            // Result set get the result of the SQL query
+			            rs = st.executeQuery("select * from participant");
+			                  
+			            writeResultSet(rs);
 
-	} 
-
-
+			            // PreparedStatements can use variables and are more efficient
+			            PreparedStatement preparedStatement = cn .prepareStatement("insert into participant ( ?, ?, ?, ? , ?, ?, ?, ? )");
+			               
+			            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
+			            // Parameters start with 1
+			            preparedStatement.setString(1, p1.getFirstName());
+			            preparedStatement.setString(2, p1.getLastName());
+			            preparedStatement.setString(3, p1.getUsername());
+			            preparedStatement.setDate(6, new java.sql.Date(2009, 12, 11));
+			            preparedStatement.setString(5, p1.getEmail());
+			            preparedStatement.setString(4, p1.getPassword());
+			            preparedStatement.setInt(8, p1.getMobileNumber());
+			            preparedStatement.setString(7, p1.getAddress());
+			            preparedStatement.executeUpdate();
+				  
+			   } catch(SQLException e) {
+				   System.out.println("errore: " + e.getMessage());
+			   } //fine try-catch  
+			   
+			   
+			   //ha dei metodi per leggere/scrivere e modificare il db registration_system
+			   
+			   
+			   cn.close();
+			   System.out.println("connection terminated"); 
+		}
+		
+		
+		
+		private void writeResultSet(ResultSet rs) {
+			// TODO Auto-generated method stub
+			
+		}
+		public static void main(String[] args) throws SQLException {
+			
+			Participant p1 = new Participant ();
+			p1.setFirstName("crisele");
+			p1.setLastName("ARIOLA");
+			p1.setUsername("crisele05");
+			p1.setPassword("password");
+			p1.setEmail("crisele05@gmail.com");
+			p1.setMobileNumber(380);
+			p1.setAddress("via Rismondo 72");
+		
+			p1.setBirthday(java.sql.Date.valueOf("2013-09-04"));
+			
+			
+			
+			
+			System.out.println(p1.getBirthday());
+			
+			
+			p1.newParticipant(p1);
+		}
+			   
 }
+	
+
+
