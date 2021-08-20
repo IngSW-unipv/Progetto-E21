02d8c5679e5be2d32b95a4c3a76b2a869e1fc297
johnhax08 <80514531+johnhax08@users.noreply.github.com>
2021-08-20 13:53:48 +0200

    Add files via upload
    aggiunto test JUnit

---
+++ src/test/java/Progetto/E21/AuctionHouseTest.java
@@ -0,0 +1,66 @@
+package Progetto.E21;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import auction.*;
+import utilities.*;
+import user.*;
+
+/**
+ * Class for JUnit test on the AuctionHouse class
+ * @see AuctionHouse
+ * @see Auction
+ * @see Item
+ * @see Lot
+ * @see Team
+ */
+public class AuctionHouseTest {
+	private static AuctionHouse house;
+	private static Auction auction;
+	
+	private static void init() {
+		Item item1 = new Item("item1", "1st item", "item1.png", 0);
+		Item item2 = new Item("item2", "2nd item", "item2.png", 1);
+		Item item3 = new Item("item3", "3rd item", "item3.png", 2);
+		Item item4 = new Item("item4", "4th item", "item4.png", 3);
+		Lot lot1 = new Lot("lot1", "1st lot", 0);
+		Lot lot2 = new Lot("lot2", "2nd lot", 1);
+		lot1.addItem(item1);
+		lot1.addItem(item2);
+		lot2.addItem(item3);
+		lot2.addItem(item4);
+		auction = new Auction("auction123", "owner", "user1", null, null, 100.0, 10.0, 1.0, 100, true);
+		auction.addLot(lot1);
+		auction.addLot(lot2);
+		house = new AuctionHouse("Auction House");
+	}
+	
+    @Test
+    public void placeBidTest() {
+    	init();
+    	try {
+			house.registerAuctionToDB(100, auction);
+			house.placeBid(100, "100");
+			auction = house.getAuction("100");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	assertEquals(110.0, auction.getHighestBid(), 0);
+    }
+    
+    @Test
+    public void userDeleteTest() {
+    	init();
+    	Participant p1 = new Participant("bhon1", "bhos1", "a@a.com", "bhobho1", "bho", null, "12345");
+    	Participant p2 = new Participant("bhon2", "bhos2", "a@a.com", "bhobho2", "bho", null, "6789");
+    	try {
+			house.registerParticipantToDB(p1);
+			house.registerParticipantToDB(p2);
+			house.deleteParticipant(p2);
+			assertEquals(1, house.getAllParticipant().size(), 0);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+}
